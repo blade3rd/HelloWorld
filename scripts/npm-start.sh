48e849b693a85b25deef7d613f71559156051c34
Anthony Tran <anthony.tran@mindtree.com>
2019-03-18 03:55:16 -0400

    update

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo node /app.js > /dev/null 2> /dev/null < /dev/null &
+sudo ufw allow 80/tcp
+sudo ufw allow 8080/tcp
+sudo ufw allow 3000/tcp
+sudo node app.js > /dev/null 2> /dev/null < /dev/null &
